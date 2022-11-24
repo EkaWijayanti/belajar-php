9519b958cb6febfca036a997d8a5621df5d9cc88
Eka Wijayanti <ekawijayanti690@gmail.com>
2022-11-25 01:53:43 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Eka";
+    $umur = "22";
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun,<br>";
+        
+    $namaAdik = "Keke";
+    $umurAdik = "10";
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
